5dfbaff4079dee0bd7ea3957e5b4bd63ebbbc517
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-10 15:04:43 +0200

    add post and get tests

--- apps/balance-service/test/balance-service.e2e-spec.ts
+++ apps/balance-service/test/balance-service.e2e-spec.ts
@@ -8,9 +8,11 @@ import testUtils from './testUtils';
 describe('BalanceServiceController (e2e)', () => {
   let app: INestApplication;
   let req: TestAgent;
+  const route: string = '/balance';
+  const userId = testUtils.getRandomUuid();
 
   //codes
-  const { OK } = testUtils.statusCode.SUCCESS;
+  const { OK, CREATED } = testUtils.statusCode.SUCCESS;
   const { BAD_REQUEST, FORBIDDEN } = testUtils.statusCode.ERROR;
 
   beforeEach(async () => {
@@ -23,53 +25,107 @@ describe('BalanceServiceController (e2e)', () => {
     req = request(app.getHttpServer());
   });
 
+  describe('.POST /balance', () => {
+    it('authorization', async () => {
+      const res = await req.post(route).send({});
+      expect(res.statusCode).toBe(FORBIDDEN);
+    });
+
+    it('.POST validation', async () => {
+      const invalidBody = [
+        undefined,
+        {},
+        //missing params
+        { amount: 5 },
+        { coin: 'bit coin' },
+        //wrong types
+        { amount: true, coin: 'bit coin' },
+        //wrong parameters:
+        { amount: 2, coin: 'bit coin', michal: true },
+      ];
+
+      await Promise.all(
+        invalidBody.map(async (body) => {
+          const res = await req.post(route).send(body).set('X-User-ID', userId);
+          return expect(res.statusCode).toBe(BAD_REQUEST);
+        }),
+      );
+    });
+
+    it('basic .POST', async () => {
+      const expectedData = {
+        coin: 'bitcoin',
+        amount: 2,
+      };
+
+      const res = await req
+        .post(route)
+        .send(expectedData)
+        .set('X-User-ID', userId);
+
+      testUtils.testResponse(res, CREATED, expectedData);
+    });
+  });
+
   describe('.GET /balance', () => {
     it('authorization', async () => {
-      const res = await req.get('/balance');
+      const res = await req.get(route);
       expect(res.statusCode).toBe(FORBIDDEN);
     });
 
     it('.GET validation', async () => {
       //not in uuid format
-      const res = await req.get('/balance').set('X-User-ID', 'not valid uid');
+      const res = await req.get(route).set('X-User-ID', 'not valid uid');
       expect(res.statusCode).toBe(BAD_REQUEST);
     });
 
-    it('basic .GET assets', () => {});
-    it('.GET user with no assets', () => {});
-    it('.GET on non existing userId', () => {});
+    it('basic .GET assets', async () => {
+      const expectedData = {
+        coin: 'bitcoin',
+        amount: 2,
+        id: 0,
+      };
+
+      const postRes = await req
+        .post(route)
+        .send({ coin: expectedData.coin, amount: expectedData.amount })
+        .set('X-User-ID', userId);
+
+      testUtils.testResponse(postRes, CREATED, expectedData);
+      expectedData.id = postRes.body.id;
+
+      const getRes = await req.get(route).set('X-User-ID', userId);
+      testUtils.testResponse(getRes, OK, [expectedData]);
+    });
+
+    it('.GET user with no assets', async () => {
+      const res = await req
+        .get(route)
+        .set('X-User-ID', testUtils.getRandomUuid());
+
+      testUtils.testResponse(res, OK, []);
+    });
   });
 
   describe('.GET /balance/total', () => {
     it('authorization', async () => {
-      const res = await req.get('/balance/total');
+      const res = await req.get(`${route}/total`);
       expect(res.statusCode).toBe(FORBIDDEN);
     });
 
-    it('.GET /total validation', () => {});
-    it('basic .GET /total', () => {});
-    it('.GET /total on non existing userId', () => {});
-  });
-
-  describe('.POST /balance', () => {
-    it('authorization', async () => {
-      const res = await req.post('/balance').send({});
-      expect(res.statusCode).toBe(FORBIDDEN);
-    });
-
-    it('.POST validation', () => {});
-    it('basic .POST', () => {});
-    it('.POST on non existing userId', () => {});
+    it('.GET /total validation', async () => {});
+    it('basic .GET /total', async () => {});
+    it('.GET /total on non existing userId', async () => {});
   });
 
   describe('.DELETE /balance', () => {
     it('authorization', async () => {
-      const res = await req.delete('/balance/1');
+      const res = await req.delete(`${route}/1`);
       expect(res.statusCode).toBe(FORBIDDEN);
     });
 
-    it('.DELETE validation', () => {});
-    it('basic .DELETE', () => {});
-    it('.DELETE on non existing asset', () => {});
+    it('.DELETE validation', async () => {});
+    it('basic .DELETE', async () => {});
+    it('.DELETE on non existing asset', async () => {});
   });
 });
