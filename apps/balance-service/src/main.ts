9da58660b896645fa277ee4a461c956948c9c11c
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-10 18:32:52 +0200

    TODO

--- apps/balance-service/src/main.ts
+++ apps/balance-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { BalanceServiceModule } from './balance-service.module';
 
 async function bootstrap() {
+  //TODO: the file should be generated here if not exist
   const app = await NestFactory.create(BalanceServiceModule);
   await app.listen(process.env.port ?? 3000);
 }
