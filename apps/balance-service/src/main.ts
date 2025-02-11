464c5778351dd67f26f44adc554078fb6922eaf9
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-11 15:33:49 +0200

    add tODO

--- apps/balance-service/src/main.ts
+++ apps/balance-service/src/main.ts
@@ -3,12 +3,8 @@ import { BalanceServiceModule } from './balance-service.module';
 import { CBSLogging } from '@app/shared/logging/logging.controller';
 
 async function bootstrap() {
-  //TODO: the file should be generated here if not exist
-  const app = await NestFactory.create(BalanceServiceModule, {
-    bufferLogs: true,
-  });
-  app.useLogger(app.get(CBSLogging));
-
+  const app = await NestFactory.create(BalanceServiceModule);
+  //TODO: config
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
