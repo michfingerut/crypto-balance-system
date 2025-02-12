92b8af7ed5cb0956b105b93d74fb4a33d9fa7e85
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-12 17:04:10 +0200

    fix any type

--- libs/shared/src/logging/logging.service.ts
+++ libs/shared/src/logging/logging.service.ts
@@ -2,11 +2,11 @@ import { ConsoleLogger, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CBSLogging extends ConsoleLogger {
-  error(message: any, stack?: string, context?: string) {
+  error(message: string, stack?: string, context?: string) {
     super.error(message, stack, context);
   }
 
-  log(message: any, context?: string) {
+  log(message: string, context?: string) {
     super.log(message, context);
   }
 }
