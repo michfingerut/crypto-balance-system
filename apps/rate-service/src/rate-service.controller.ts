754289fa26a2d0b8d8d1e563a1159cd124e106d9
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-12 14:23:40 +0200

    add logging, remove TODOs

--- apps/rate-service/src/rate-service.controller.ts
+++ apps/rate-service/src/rate-service.controller.ts
@@ -1,20 +1,21 @@
 import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
 
-//import { CBSLogging } from '@app/shared/logging/logging.service';
+import { CBSLogging } from '@app/shared/logging/logging.service';
 
 import { RateService } from './rate-service.service';
 import { GetRateDTO } from './dto/get-rate.dto';
 
 @Controller('rate')
 export class RateServiceController {
+  private readonly logger = new CBSLogging(RateServiceController.name);
   constructor(private readonly rateService: RateService) {}
 
   @Get()
   async getRate(@Query(ValidationPipe) query: GetRateDTO) {
-    //TODO need to validate that coin exist
-    //TODO: logger
     const { coin, vs_coin } = query;
 
-    return this.rateService.getRate(coin, vs_coin);
+    const res = this.rateService.getRate(coin, vs_coin);
+    this.logger.log(`get rate ${coin}-${vs_coin}`);
+    return res;
   }
 }
