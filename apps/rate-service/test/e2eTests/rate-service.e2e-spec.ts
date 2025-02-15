5a2613abedd23625a59caa491c343fc5937341fa
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-02-15 17:19:54 +0200

    tests work

--- apps/rate-service/test/e2eTests/rate-service.e2e-spec.ts
+++ apps/rate-service/test/e2eTests/rate-service.e2e-spec.ts
@@ -1,41 +1,16 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
 import { getRandomUuid, statusCode } from '../testUtils/index';
 
-// jest.mock('@app/shared/logging/logging.service', () => {
-//   return {
-//     CBSLogging: jest.fn().mockImplementation(() => ({
-//       log: jest.fn(),
-//       error: jest.fn(),
-//       warn: jest.fn(),
-//       debug: jest.fn(),
-//       verbose: jest.fn(),
-//       setContext: jest.fn(),
-//     })),
-//   };
-// });
 
 describe('RateServiceController (e2e)', () => {
-  let app: INestApplication;
-  let req: TestAgent;
+  let req: TestAgent = request('http://localhost:3002');
   const route: string = '/rate';
   const userId = getRandomUuid();
 
   //codes
   const { OK } = statusCode.SUCCESS;
-
-  beforeEach(async () => {
-    // const moduleFixture: TestingModule = await Test.createTestingModule({
-    //   imports: [RateServiceModule],
-    // }).compile();
-
-    // app = moduleFixture.createNestApplication();
-    // await app.init();
-    req = request('http://localhost:3002');
-  });
 
   it('basic e2e .GET rate', async () => {
     const coin = 'bitcoin';
